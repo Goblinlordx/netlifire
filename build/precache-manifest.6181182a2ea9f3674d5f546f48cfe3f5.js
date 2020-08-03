9374f6017ef5f362d26ca693d039d0d9829da202
Benjamin Baldivia <goblinlordx@gmail.com>
2020-08-03 22:24:59 +0900

    Move to using gatsby

---
+++ build/precache-manifest.6181182a2ea9f3674d5f546f48cfe3f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8289675da8785e35f4ccb1429284982",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1101196a9c2a523d3d78",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b93929bfe08b4497380",
+    "url": "/static/js/2.a50da409.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a50da409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1101196a9c2a523d3d78",
+    "url": "/static/js/main.6323b1ad.chunk.js"
+  },
+  {
+    "revision": "2f50b14c7fdbf8ee4a27",
+    "url": "/static/js/runtime-main.e31862ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
